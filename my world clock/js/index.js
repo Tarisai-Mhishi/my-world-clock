cb1d377883b850e344d3667524dec54211e4fc11
Tarisai-Mhishi <tarisaimhishi1@gmail.com>
2024-12-15 17:59:58 +0000

    added javascript for functionality

--- my world clock/js/index.js
+++ my world clock/js/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let harareElement = document.querySelector("#harare");
+  let harareDateElement = document.querySelector("#harare .date");
+  let harareTimeElement = document.querySelector("#harare .time");
+  let harareTime = moment().tz("Africa/Harare");
 
+  harareDateElement.innerHTML = harareTime.format("MMMM Do YYYY");
+  harareTimeElement.innerHTML = harareTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = document.querySelector("#sydney .date");
+  let sydneyTimeElement = document.querySelector("#sydney .time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
